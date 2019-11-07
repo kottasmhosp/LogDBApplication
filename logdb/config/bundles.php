8631202659431ac2273a6f442814545f17f98c15
Thomas Kottas <kottas@m-hospitality.com>
2019-11-07 17:08:27 +0200

    added assets folder and required dependencies

--- logdb/config/bundles.php
+++ logdb/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
